4474f1487fce70215ff3728dd5933f20f7330b58
Makstak <kvasia08.96@gmail.com>
2022-12-18 18:01:24 +0300

    homework 06

---
+++ hw06/task_01.py
@@ -0,0 +1,52 @@
+def convSmToInches(chislo):
+    convert = chislo * 0.39
+    return convert
+
+def convInchesToSm(chislo):
+    convert = chislo * 2.54
+    return convert
+
+def convMilesToKilometers(chislo):
+    convert =  chislo * 1.609344
+    return convert
+
+def convKiometrsToMiles(chislo):
+    convert = chislo * 0.621371
+    return convert
+
+def convFuntsToKilograms(chislo):
+    convert = chislo * 0.45359237
+    return convert
+
+def convKilogramsToFunts(chislo):
+    convert = chislo * 2.20462262185
+    return convert
+
+def convUnciiToGrams(chislo):
+    convert = chislo * 28.34952
+    return convert
+
+def convGramsToUncii(chislo):
+    convert = chislo / 28.34952
+    return convert
+
+def convGallonsToLitres(chislo):
+    convert = chislo * 0.264172
+    return convert
+
+def convLitresToGallons(chislo):
+    convert = chislo * 3.785412
+    return convert
+
+def convLitresToPinta(chislo):
+    convert = chislo * 0.5683
+    return convert
+
+def convPintToLitres(a):
+    convert = a * 0.473176
+    return convert
+
+
+
+
+print(convPintToLitres(5))
