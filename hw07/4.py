2e9f2220f025d0f7667f439229ec68b0133c2f2e
Makstak <kvasia08.96@gmail.com>
2023-01-04 22:28:16 +0300

    first try

--- hw07/4.py
+++ hw07/4.py
@@ -7,13 +7,20 @@
 # В качестве результат вывести на экран значение
 # функции y при каждом x из указанного диапазона.
 
-
+def func(num1:int, num2:int) -> list:
+    result1 = list(map(lambda x: x**2, range(num1, num2+1)))
+    result2 = list(map(lambda x: 2*abs(x)-1, range(num1-1,num2)))
+    result3 = list(map(lambda x: 2*x, range(num1-1, num2 + 1)))
+    return (result1, result2, result3)
 
 
 def main():
-    print(list(map(lambda x: x**2, range(-5,6))))
-    print(list(map(lambda x: 2*abs(x)-1, range(-10,-5))))
-    print(list(map(lambda x: 2*x, range(6,11))))
+    num1 = int(input())
+    num2 = int(input())
+    result = func(num1, num2)
+    for x in result:
+        print(x)
+
 
 if __name__ == "__main__":
     main()
