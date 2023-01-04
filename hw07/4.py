f21fc7b6f37c76282b8d5a05fc6f25a732374f69
Makstak <kvasia08.96@gmail.com>
2023-01-04 23:30:54 +0300

    4 задача

--- hw07/4.py
+++ hw07/4.py
@@ -7,20 +7,41 @@
 # В качестве результат вывести на экран значение
 # функции y при каждом x из указанного диапазона.
 
-def func(num1:int, num2:int) -> list:
-    result1 = list(map(lambda x: x**2, range(num1, num2+1)))
-    result2 = list(map(lambda x: 2*abs(x)-1, range(num1-1,num2)))
-    result3 = list(map(lambda x: 2*x, range(num1-1, num2 + 1)))
-    return (result1, result2, result3)
+def func(num1:int, num2: int) -> list:
+    list = []
+    for x in range(num1, num2):
+        if x<-5:
+            y = 2*abs(x)-1
+            list.append(y)
+        elif -5<=x and x<=5:
+            y = x**2
+            list.append(y)
+        else :
+            y = 2*x
+            list.append(y)
+    return list
 
 
 def main():
     num1 = int(input())
     num2 = int(input())
     result = func(num1, num2)
-    for x in result:
-        print(x)
+    print(result)
 
 
 if __name__ == "__main__":
     main()
+
+
+# def func(num1:int, num2:int) -> list:
+#     result = [2*abs(x)-1 for x in range(num1, num2+1) if x<-5]
+#     result +=[x**2 for x in range(num1, num2+1) if -5<=x and x<=5]
+#     result +=[2*x for x in range(num1, num2+1) if x > 5]
+#     return (result)
+#
+#
+# def main():
+#     num1 = int(input())
+#     num2 = int(input())
+#     result = func(num1, num2)
+#     print(result)
