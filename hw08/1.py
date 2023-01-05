c90dd980ce87ea493670f7e427a75f67fd2d3582
Makstak <kvasia08.96@gmail.com>
2023-01-05 23:30:45 +0300

    changes 1.py

--- hw08/1.py
+++ hw08/1.py
@@ -3,25 +3,44 @@
 # предварительную проверку данных - удалять все
 # четные элементы из списка.
 
+# import random
+#
+# def decor_func(func):
+#     elem = func()
+#     print(elem)
+#     def elem_check(elem):
+#         return list(filter(lambda x: x % 2, elem))
+#     return elem_check(elem)
+#
+#
+#
+# def main():
+#
+#     @decor_func
+#     def list_elements():
+#         return [random.randint(0, 100) for x in range(0, 20)]
+#
+#     result = list_elements
+#     print(result)
+#
+# if __name__ == "__main__":
+#     main()
+
 import random
 
 def decor_func(func):
-    elem = func()
-    print(elem)
     def elem_check(elem):
         return list(filter(lambda x: x % 2, elem))
-    return elem_check(elem)
-
-
+    return elem_check
 
 def main():
 
     @decor_func
-    def list_elements():
-        return [random.randint(0, 100) for x in range(0, 20)]
+    def list_elements(list):
+        Pass
 
-    result = list_elements
-    print(result)
+    list_elem = [1,3,4,5,2,34,4,65,67,2,31,243,5,46,23,1,232,1,454,562,2,3]
+    print(list_elements(list_elem))
 
 if __name__ == "__main__":
     main()
