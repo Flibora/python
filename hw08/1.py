46eb7fc2db538605bdbb1bf2a33cc4ea96d314c3
Makstak <kvasia08.96@gmail.com>
2023-01-06 01:16:53 +0300

    2.py

--- hw08/1.py
+++ hw08/1.py
@@ -3,44 +3,27 @@
 # предварительную проверку данных - удалять все
 # четные элементы из списка.
 
-# import random
-#
-# def decor_func(func):
-#     elem = func()
-#     print(elem)
-#     def elem_check(elem):
-#         return list(filter(lambda x: x % 2, elem))
-#     return elem_check(elem)
-#
-#
-#
-# def main():
-#
-#     @decor_func
-#     def list_elements():
-#         return [random.randint(0, 100) for x in range(0, 20)]
-#
-#     result = list_elements
-#     print(result)
-#
-# if __name__ == "__main__":
-#     main()
+from functools import reduce
 
-import random
-
-def decor_func(func):
-    def elem_check(elem):
-        return list(filter(lambda x: x % 2, elem))
+def decor_func(function_to_decorate):
+    def elem_check(arg):
+        new_arg = filter(lambda x: x % 2, arg)
+        return function_to_decorate(new_arg)
     return elem_check
 
 def main():
 
     @decor_func
-    def list_elements(list):
-        Pass
+    def list_elements(list_el):
+        return reduce(lambda x,n: x*n, list_el)
 
-    list_elem = [1,3,4,5,2,34,4,65,67,2,31,243,5,46,23,1,232,1,454,562,2,3]
-    print(list_elements(list_elem))
+    @decor_func
+    def list_elem(list_el):
+        return reduce(lambda x,n: x+n, list_el)
+
+    print(list_elements([1,2,3,4,5,6]))
+    print(list_elem([1,2,3,4,5,6]))
+
 
 if __name__ == "__main__":
     main()
